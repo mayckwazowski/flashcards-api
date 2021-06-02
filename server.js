66be2adfaae82281265609abe70437cda9788f2e
Mayck Cipriano <mayck.cipra@gmail.com>
2021-06-02 16:28:51 -0300

    Desenvolvimento da api de flashcards - Aula 3

--- server.js
+++ server.js
@@ -1,31 +1,99 @@
 const express = require("express");
 const app = express();
-const banco = require("./dados.mock.json");
+const banco = require("./mock/banco")
+
 
 app.use(express.json());
 
-// CRUD - CREATE READ UPDATE DELETE
+// GET /api/colecoes - Esse endpoint deverá listar todas as coleções do sistema.
 app.get( "/api/colecoes", function( req, res){
-    res.json( banco.colecoes );
+    res.json( banco.todasColecoes() );
 } );
 
+//GET /api/colecoes/:id - Esse endpoint deverá retornar os dados da coleção selecionada pelo parâmetro id.
 app.get( "/api/colecoes/:id", function( req, res){
     const { id } = req.params;
-    const colecao = banco.colecoes.find( col => col.colecaoId == id );
+    const colecao = banco.selecionaColecao( id );
 
     if( !colecao ) res.status(204).json();
 
     res.json( colecao );
 } );
 
-app.post( "/api/colecoes", function( req, res){
-    const { descricao, publico } = req.body;
-    const colecaoId = banco.seqColecaoId++;
+// GET /api/colecoes/flashcards/:id- Esse endpoint deverá listar todos os flashcards da coleção passada pelo endereço, 
+// identificado pela parâmetro id.
+app.get( "/api/colecoes/flashcards/:id", function( req, res){
+    const { id } = req.params;
+    const flashcard = banco.selecionaFlashCard( id );
 
-    // salvar
+    if( !flashcard ) res.status(204).json();
+
+    res.json( flashcard );
+} );
+
+// GET /api/flashcards/:id- Esse endpoint deverá exibir os dados somente do flashcard selecionado, identificado pela variável id.
+app.get( "/api/flashcards/:id", function( req, res){
+    const { id } = req.params;
+    const flashcard = banco.selecionaFlashCard( id );
+
+    if( !flashcard ) res.status(204).json();
+
+    res.json( flashcard );
+} );
+
+// POST /api/colecoes - Esse endpoint deverá criar uma nova coleção com os dados enviada pelo client, por meio do body. 
+app.post( "/api/colecoes", function( req, res ){
+    const { descricao, publico } = req.body;
+    banco.salvar( { descricao, publico } );
+    res.json( { descricao, publico } );
+} );
+
+// PUT /api/colecoes/:id - Esse endpoint deverá selecionar uma coleção, por meio do parâmetro id, e atualizar a mesma com os dados enviados pelo valores enviados por meio do body.
+app.put( "/api/colecoes", function( req, res ){
+    const { colecaoId, descricao, publico } = req.body;
+    banco.salvar( { colecaoId, descricao, publico } );
+
+    const colecao = banco.selecionaColecao( colecaoId );
+
+    if( !colecao ) res.status(204).json();
 
     res.json( colecao );
 } );
+
+// DELETE /api/colecoes/:id - Esse endpoint deverá selecionar uma coleção, por meio do parâmetro id, e excluir o registro na base de dados.
+app.delete( "/api/colecoes/:id", function( req, res){
+    const { id } = req.params;
+    const colecao = banco.selecionaColecao( id );
+
+    banco.apagar( colecao );
+
+    res.json( banco.todasColecoes() );
+} );
+
+// POST /api/flashcards - Esse endpoint deverá criar um novo flashcard com os dados enviada pelo client, por meio do body.
+app.post( "/api/colecoes", function( req, res ){
+    const { colecaoId, frente, verso } = req.body;
+    banco.salvar( { colecaoId, frente, verso } );
+    res.json( { descricao, publico } );
+} );
+
+// PUT /api/flashcards/:id - Esse endpoint deverá selecionar um flashcard, por meio do parâmetro id, e atualizar a mesma com os dados enviados pelo valores enviados por meio do body.
+
+// DELETE /api/flashcards/:id - Esse endpoint deverá selecionar um flashcard, por meio do parâmetro id, e excluir o registro na base de dados.
+app.delete( "/api/flashcards/:id", function( req, res){
+    const { id } = req.params;
+    const flashcard = banco.selecionaFlashCard( id );
+
+    if( !flashcard ) {
+        res.status(204).json();
+        return;
+    }else{
+        banco.apagar( flashcard );
+
+        res.json( flashcard );    
+    }
+} );
+
 
 app.listen( 3000, function(){
     console.log('Server rodando');
